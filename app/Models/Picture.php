76b86dbacb26135acba2aa49714fe590a4d11e44
jerrysanguyo <jsanguyo1624@gmail.com>
2025-02-12 02:05:53 +0800

    added picture model and migration

---
+++ app/Models/Picture.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Picture extends Model
+{
+    use HasFactory;
+    protected $table = 'pictures';
+    protected $fillable = [
+        'file_name',
+        'file_path',
+        'picture_name',
+        'type',
+        'created_by',
+        'updated_by'
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+}
---
+++ database/migrations/2025_02_11_173102_create_pictures_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->string('picture_name');
+            $table->enum('type', ['item', 'itemDetail']);
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pictures');
+    }
+};
